f947ccb717ab444367a94f24848082bffefcc730
Javius Loh Jingwei <145333430+javiusloh@users.noreply.github.com>
2024-08-05 15:53:31 +0800

    Updated MembershipView

--- SPEA/MembershipView.swift
+++ SPEA/MembershipView.swift
@@ -18,10 +18,7 @@ struct Membership: Identifiable {
 struct MembershipView: View {
         
     @State var memberList = [
-        Membership(name: "Beary Fun Gym", valid: "- Valid until 31 July 2024", details: "- 20% off One (1) Term Gymnastics Programme Fee at any one of the outlets: \nBukit Batok / Changi / Choa Chu Kang / Katong", logo: "BearyFunGym"),
-        Membership(name: "Chiropractic Health & Wellness Clinic", valid: "- Valid until 31 December 2024", details: "- One time $54.00 discount on their first consultation visit", logo: "ChiropracticHealth&Wellness"),
-        Membership(name: "Guang An TCM", valid: "- Valid until 31 July 2024", details: "- 15% off Sport Recovery Tuina \n- 10% off all other body / foot massage \n\n* Booking in advance is required, subject to availability", logo: "GuangAnTCM"),
-        Membership(name: "Singapore Raffles Music College", valid: "- Valid until 28 July 2024", details: "- SRMC will offer one (1) place for full sponsorship of the programme fees of up to $20,500 in each calendar year and two (2) places for partial sponsorship of the programme fees of up to $10,000 for each place in each calendar year to SPEA Members and their immediate family members who are enrolled as new students of SRMC. \n\n- Applicable to the following Programmes only;\no   Certificate in Music\no   Diploma in Music\no   Advanced Diploma in Music\no   Advanced Diploma in Dance \n\n- Eligibility criteria applies.", logo: "SingaporeRafflesMusicCollege"),
+        Membership(name: "Chiropractic Health & Wellness Clinic", valid: "- Valid until 31 December 2024", details: "- One time $54.00 discount on their first consultation visit", logo: "ChiropracticHealth&Wellness")
     ]
     @State private var settingsDetent = PresentationDetent.medium
     @State private var showingMembership = false
